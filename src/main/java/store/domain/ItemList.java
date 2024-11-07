a36fb0cd1c70cbf053e78faa64567b9b09ebd4e3
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-07 21:36:26 +0900

    feat : 상품 리스트 저장 모델 생성 및 구현

---
+++ src/main/java/store/domain/ItemList.java
@@ -0,0 +1,11 @@
+package store.domain;
+
+import java.util.List;
+
+public class ItemList {
+    private final List<Item> items;
+
+    public ItemList(List<Item> items) {
+        this.items = items;
+    }
+}
