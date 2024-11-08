108e3505619e057ef37080e34a559592470248e0
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-09 01:37:03 +0900

    refactor : 물건 목록 출력 수정

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -2,18 +2,24 @@ package store.view;
 
 import store.dto.ItemDto;
 
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class OutputView {
     public static void printItemList(List<ItemDto> items) {
         System.out.println("안녕하세요. W편의점입니다.");
-        System.out.println("현재 보유하고 있는 상품입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.\n");
+
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.KOREA);
 
         for (ItemDto item : items) {
-            String promotion = item.getItemPromotion() != null ? item.getItemPromotion() : "";
-            String etc = item.getItemEtc() != null ? item.getItemEtc() : "";
+            if (item.getItemPromotion() == null) {
+                System.out.printf("%s %s원 %d개 %n", item.getItemName(), numberFormat.format(item.getItemPrice()), item.getItemQuantity());
+            } else {
+                System.out.printf("%s %s원 %d개 %s %n", item.getItemName(), numberFormat.format(item.getItemPrice()), item.getItemQuantity(), item.getItemPromotion());
 
-            System.out.printf("%s %d %d개 %s %s%n", item.getItemName(), item.getItemPrice(), item.getItemQuantity(), promotion, etc);
+            }
         }
     }
 }
