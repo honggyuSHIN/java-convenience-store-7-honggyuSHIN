457e610e1fce6631fdd60e0732698691945a7a8f
honggyu1233@naver.com <honggyu1233@naver.com>
2024-11-08 21:31:08 +0900

    feat : 아이템 dto 클래스 추가

---
+++ src/main/java/store/dto/ItemDto.java
@@ -0,0 +1,37 @@
+package store.dto;
+
+public class ItemDto {
+    private final String itemName;
+    private final int itemPrice;
+    private final int itemQuantity;
+    private final String itemPromotion;
+    private final String itemEtc;
+
+    public ItemDto(String itemName, int itemPrice, int itemQuantity, String itemPromotion, String itemEtc) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemQuantity = itemQuantity;
+        this.itemPromotion = itemPromotion;
+        this.itemEtc = itemEtc;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public String getItemPromotion() {
+        return itemPromotion;
+    }
+
+    public String getItemEtc() {
+        return itemEtc;
+    }
+}
